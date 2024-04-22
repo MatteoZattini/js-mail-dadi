2105903ff7ea943d2cb8dca350665802979ae3f3
MatteoZattini <matteozattini@hotmail.com>
2024-04-22 18:25:13 +0200

    added start of mail script

---
+++ mail.js
@@ -0,0 +1,20 @@
+let listaEmail = [
+    "matteo@hotmail.com",
+    "zattini@gmail.com",
+    "matteozattini@gmail.com",
+    "matteo@gmail.com",
+]
+
+
+for (let i = 0; i < listaEmail.length; i++) {
+    console.log(listaEmail[i])
+}
+
+let myButton = document.getElementById('btn')
+let utenteEmail = document.getElementById("mail")
+
+myButton.addEventListener('click', function() {
+    console.log(utenteEmail.value)
+})
+
+
